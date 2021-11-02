9d7aeb41c695786426ee162eb3189da025008860
leon <leon7502045@gmail.com>
2021-11-02 12:16:14 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Projet Eligibilite</title>
+    </head>
+    
+    <body>
+        <h1>Goblet Inc.</h1>
+        
+        <p>
+            Cette page contient du code HTML avec des balises PHP.<br />
+            <?php 
+                echo "Texte via php"; 
+            ?>
+        </p>
+        
+        <h3>Sélection de café :</h3>
+        <ul>
+        <li>Expresso</li>
+        <li>Allongé</li>
+        <li>Double Expresso</li>
+        </ul>
+
+    </body>
+</html>
